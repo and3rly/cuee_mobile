b1a03fd28be31d045632c86ac3235a67193f3a84
anderly <anderly>
2024-05-18 15:05:58 -0600

    Comunicacion desde login, generar profroma cuando el consumo es 0

--- app/src/main/java/com/example/cuee_mobile/controladores/Menu.java
+++ app/src/main/java/com/example/cuee_mobile/controladores/Menu.java
@@ -153,7 +153,7 @@ public class Menu extends PBase {
                     break;
                 case 5:
                     lpendientes = lectura.getLecturasPendientes();
-                    lpendientes.clear();
+                    //lpendientes.clear();  Quitar comentario para realizar pruebas en envio
 
                     if (lpendientes.size() > 0) {
                         helper.toast("Tiene usuarios pendientes de ingreso lectura.");
